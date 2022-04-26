fb0d521d301d41fa4685c52305d975d82300f692
Abhinav-coder368 <76556517+Abhinav-coder368@users.noreply.github.com>
2022-04-26 22:11:50 +0530

    Add files via upload

---
+++ Watermelon Codeforces 800.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+     int k;
+     std::cin >>k;
+     if(k%2==0 && k>2){
+         
+             cout<<"YES";
+     }
+     else
+        cout<<"NO";
+       
+     
+	return 0;
+}
